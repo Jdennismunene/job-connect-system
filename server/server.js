abed1c7f90284ee5acd1382cb9e51813041bf2b7
Fay-chebby <chebetf034@gmail.com>
2025-08-27 00:01:53 +0300

    Backend set up

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// load env
+dotenv.config();
+connectDB();
+
+// Initialize app
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, async () => {
+  await connectDB();
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
+module.exports = app;
