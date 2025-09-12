9732c093caf85b3a6acf0aa52cbb6604e2478c35
Fay-chebby <chebetf034@gmail.com>
2025-09-12 23:25:00 +0300

    done

--- server/Models/user.js
+++ server/Models/user.js
@@ -24,12 +24,4 @@ const userSchema = new mongoose.Schema({
   verificationTokenExpiresAt: Date,
 });
 
-// // Hash password before saving it to the database
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
-
 module.exports = mongoose.model("user", userSchema);
