aaf51eac861310772327b8f89f44e8538a1a2cf7
Fay-chebby <chebetf034@gmail.com>
2025-09-22 03:36:08 +0300

    Getting Jobs by id

--- server/Controllers/jobController.js
+++ server/Controllers/jobController.js
@@ -1,7 +1,9 @@
 const Job = require("../Models/job");
 const User = require("../Models/user");
 const Application = require("../Models/Application");
-const Savedjob = require("../Models/savedjobs");
+const SavedJob = require("../Models/SavedJob");
+const job = require("../Models/job");
+const { application } = require("express");
 
 exports.createJob = async (req, res) => {
   try {
@@ -17,6 +19,128 @@ exports.createJob = async (req, res) => {
     res.status(201).json(job);
   } catch (err) {
     console.error("Error creating job:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+exports.getJobs = async (req, res) => {
+  const { keyword, location, category, type, minSalary, maxSalary, userId } =
+    req.query;
+
+  const query = {
+    isClosed: false,
+    ...(keyword && { title: { $regex: keyword, $options: "i" } }),
+    ...(location && { location: { $regex: location, $options: "i" } }),
+    ...(category && { category }),
+    ...(type && { type }),
+  };
+  if (minSalary || maxSalary) {
+    query.$and = [];
+
+    if (minSalary) {
+      query.$and.push({ salaryMax: { $gte: Number(minSalary) } });
+    }
+    if (maxSalary) {
+      query.$and.push({ salaryMin: { $lte: Number(maxSalary) } });
+    }
+    if (query.$and.length === 0) {
+      delete query.$and;
+    }
+  }
+
+  try {
+    const jobs = await Job.find(query).populate(
+      "company",
+      "name companyName companyLogo"
+    );
+
+    let savedJobIds = [];
+    let appliedJobStatusMap = {};
+
+    if (userId) {
+      // Saved Jobs
+      const savedJobs = await SavedJob.find({ jobseeker: userId }).select(
+        "job"
+      );
+      savedJobIds = savedJobs.map((s) => String(s.job));
+
+      // Applications
+      const applications = await Application.find({ applicant: userId }).select(
+        "job status"
+      );
+      applications.forEach((app) => {
+        appliedJobStatusMap[String(app.job)] = app.status;
+      });
+    }
+    const jobsWithExtras = jobs.map((job) => {
+      const jobIdStr = String(job._id);
+      return {
+        ...job.toObject(),
+        isSaved: savedJobs.includes(jobIdStr),
+        applicationStatus: appliedJobStatusMap[jobIdStr] || null,
+      };
+    });
+
+    res.json(jobsWithExtras);
+  } catch (err) {
+    res.status(500).json({ msg: "error message" });
+  }
+};
+
+exports.getJobsEmployer = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { role } = req.user;
+    if (role !== "employer") {
+      return res.status(403).json({ message: "Access denied" });
+    }
+    // get all jobs by employer
+    const jobs = await Job.find({ company: userId })
+      .populate("company", "name companyName companyLogo")
+      .lean();
+
+    const jobsWithApplicationCounts = await Promise.all(
+      jobs.map(async (job) => {
+        const applicationCount = await Application.countDocuments({
+          job: job._id,
+        });
+        return {
+          ...job,
+          applicationCount,
+        };
+      })
+    );
+
+    res.json(jobsWithApplicationCounts);
+  } catch (error) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getJobById = async (req, res) => {
+  try {
+    const { userId } = req.query;
+    const job = await Job.findById(req.params.id).populate(
+      "company",
+      "name companyName companyLogo"
+    );
+    if (!job) {
+      return res.status(400).json({ message: "Job not found" });
+    }
+    let applicationStatus = null;
+    if (userId) {
+      const applicant = await Application.findOne({
+        job: job._id,
+        applicant: userId,
+      }).select("status");
+      if (applicant) {
+        applicationStatus = applicationStatus;
+      }
+    }
+    res.json({
+      ...job.toObject(),
+      applicationStatus,
+    });
+  } catch (error) {
     res.status(500).json({ message: err.message });
   }
 };
